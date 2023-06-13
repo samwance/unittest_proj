5c616bf861134db61b9b851ddcd7533e01220fc5
semyon <odintsovvans@gmail.com>
2023-06-13 23:49:03 +0600

    added more tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,13 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+
+    def test_get_out_of_bounds(self):
+        self.assertEqual(arrs.get([1, 2, 3], 10, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
+
+    def test_my_slice_out_of_bounds(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], 0, 10), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -10, 2), [1, 2])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1, 2), [])
+        self.assertEqual(arrs.my_slice([], -10, 2), [])
